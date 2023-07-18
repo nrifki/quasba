ec3047fb5e199ea03583085d5bc8172715cc9875
Nada Rifki <rifki.nada@gmail.com>
2023-07-18 15:46:47 +0200

    🎨 Generate wallpaper fits size iphone11

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,18 +7,11 @@ const randomQuote = ref<{
   book: string;
   chapter: string;
   page: string;
-}>({
-  quote: "",
-  author: "",
-  book: "",
-  chapter: "",
-  page: "",
-});
+}>({ quote: "", author: "", book: "", chapter: "", page: "" });
 
 onMounted(async () => {
   const response = await fetch("/data/quotes.json");
   const quotes = await response.json();
-
   randomQuote.value = quotes[Math.floor(Math.random() * quotes.length)];
   await nextTick();
   drawQuote();
@@ -29,12 +22,47 @@ const drawQuote = () => {
   const ctx = canvas.getContext("2d");
 
   if (ctx) {
+    const gradient = ctx.createLinearGradient(0, 0, 0, canvas.height);
+    gradient.addColorStop(0, "black");
+    gradient.addColorStop(0.6, "gray");
+
+    ctx.fillStyle = gradient;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
     ctx.fillStyle = "white";
-    ctx.font = "20px Arial";
+    ctx.font = "48px Arial";
     ctx.textAlign = "center";
-    ctx.fillText(randomQuote.value.quote, canvas.width / 2, canvas.height / 2);
+
+    const y = canvas.height / 2; // Adjust to center text vertically
+    wrapText(
+      ctx,
+      randomQuote.value.quote,
+      canvas.width / 2,
+      y,
+      canvas.width - 50,
+      48
+    ); // Leave 50px padding on either side
   }
   generateImage();
+};
+
+const wrapText = (context, text, x, y, maxWidth, lineHeight) => {
+  const words = text.split(" ");
+  let line = "";
+
+  for (let n = 0; n < words.length; n++) {
+    const testLine = line + words[n] + " ";
+    const metrics = context.measureText(testLine);
+    const testWidth = metrics.width;
+    if (testWidth > maxWidth && n > 0) {
+      context.fillText(line, x, y);
+      line = words[n] + " ";
+      y += lineHeight;
+    } else {
+      line = testLine;
+    }
+  }
+  context.fillText(line, x, y);
 };
 
 const generateImage = () => {
@@ -49,8 +77,8 @@ const generateImage = () => {
 </script>
 
 <template>
-  <div>
-    <canvas id="quoteCanvas" width="500" height="500"></canvas>
+  <div class="w-full">
     <a id="downloadLink">Download Quote</a>
+    <canvas id="quoteCanvas" height="1792" width="828"></canvas>
   </div>
 </template>
