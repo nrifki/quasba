25639aab398b249b77984a176428fb35d1b8fb66
Nada Rifki <rifki.nada@gmail.com>
2023-07-18 14:49:27 +0200

    🎲 Generate random quote

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,16 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted, ref } from "vue";
+
+const randomQuote = ref({});
+
+onMounted(async () => {
+  const response = await fetch("/data/quotes.json");
+  const quotes = await response.json();
+
+  randomQuote.value = quotes[Math.floor(Math.random() * quotes.length)];
+});
+</script>
 
 <template>
-  <div></div>
+  <div>{{ randomQuote }}</div>
 </template>
