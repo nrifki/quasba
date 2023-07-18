e8d47f3a9d637d43eb0437248199891e5a178b50
Nada Rifki <rifki.nada@gmail.com>
2023-07-18 14:56:55 +0200

    🏭 generate and download img with random quote

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,56 @@
 <script setup lang="ts">
-import { onMounted, ref } from "vue";
+import { onMounted, ref, nextTick } from "vue";
 
-const randomQuote = ref({});
+const randomQuote = ref<{
+  quote: string;
+  author: string;
+  book: string;
+  chapter: string;
+  page: string;
+}>({
+  quote: "",
+  author: "",
+  book: "",
+  chapter: "",
+  page: "",
+});
 
 onMounted(async () => {
   const response = await fetch("/data/quotes.json");
   const quotes = await response.json();
 
   randomQuote.value = quotes[Math.floor(Math.random() * quotes.length)];
+  await nextTick();
+  drawQuote();
 });
+
+const drawQuote = () => {
+  const canvas = document.getElementById("quoteCanvas") as HTMLCanvasElement;
+  const ctx = canvas.getContext("2d");
+
+  if (ctx) {
+    ctx.fillStyle = "white";
+    ctx.font = "20px Arial";
+    ctx.textAlign = "center";
+    ctx.fillText(randomQuote.value.quote, canvas.width / 2, canvas.height / 2);
+  }
+  generateImage();
+};
+
+const generateImage = () => {
+  const canvas = document.getElementById("quoteCanvas") as HTMLCanvasElement;
+  const downloadLink = document.getElementById(
+    "downloadLink"
+  ) as HTMLAnchorElement;
+
+  downloadLink.href = canvas.toDataURL("image/png");
+  downloadLink.download = "quote.png";
+};
 </script>
 
 <template>
-  <div>{{ randomQuote }}</div>
+  <div>
+    <canvas id="quoteCanvas" width="500" height="500"></canvas>
+    <a id="downloadLink">Download Quote</a>
+  </div>
 </template>
